ab7baec49e7f19fceea927eda0377d86fad27c89
MSoler <caiskove@gmail.com>
2023-11-12 00:16:21 +0100

    [Gameplay] Add grass

---
+++ src/gameplay/entities/grass.gd
@@ -0,0 +1,8 @@
+extends Area2D
+
+@onready var collider : CollisionShape2D = $CollisionShape2D
+@onready var sprite : AnimatedSprite2D = $Sprite2D
+
+func _on_area_entered(_body):
+	print(_body)
+	sprite.play("rustle")
